c3350d3081d46e95605b55f35af311b3e4c717a4
ashahir <ashahir@gmail.com>
2023-02-07 21:52:52 +0800

    edit form

--- user.php
+++ user.php
@@ -43,7 +43,8 @@ if (isset($_GET['mode']) === true && in_array($_GET['mode'], $mode_allowed) === 
 }
 
 if (empty($_POST) === false) {
-	$required_fields = array('name', 'noic', 'age', 'gander', 'avatar');
+	//$required_fields = array('name', 'noic', 'age', 'gander', 'avatar');
+	$required_fields = array('name', 'noic', 'avatar');
 	foreach ($_POST as $key=>$value) {
 		if (empty($value) && in_array($key,$required_fields) === true) {
 			$errors[] = 'Ruang \'' . $key . '\' yang bertanda bintang (*) adalah wajib diisi.';
@@ -60,9 +61,15 @@ if (empty($_POST) === false) {
 			if (is_numeric($_POST['noic']) !== true) {
 				$errors[] = 'No KP hanya mengandungi nombor sahaja.';
 			}
-			if (is_numeric($_POST['age']) !== true) {
+            if (preg_match("/\\s/", $_POST['noic']) == true) {
+                $errors[] = 'No KP tidak boleh mempunyai ruang.';
+            }
+            if (strlen($_POST['noic']) <> 12) {
+                $errors[] = 'No KP anda tidak sah.';
+            }
+			/* if (is_numeric($_POST['age']) !== true) {
 				$errors[] = 'Umur hanya mengandungi nombor sahaja.';
-			}
+			} */
 		} else if ($_GET['mode'] === 'edit') {
 			if (mysqli_result($query, 0) == 1 && $row['noic'] !== $_POST['noic']) {
 				$errors[] = 'No KP \'' . $_POST['noic'] . '\' sudah didaftarkan.';
@@ -70,9 +77,15 @@ if (empty($_POST) === false) {
 			if (is_numeric($_POST['noic']) !== true) {
 				$errors[] = 'No KP hanya mengandungi nombor sahaja.';
 			}
-			if (is_numeric($_POST['age']) !== true) {
+            if (preg_match("/\\s/", $_POST['noic']) == true) {
+                $errors[] = 'No KP tidak boleh mempunyai ruang.';
+            }
+            if (strlen($_POST['noic']) <> 12) {
+                $errors[] = 'No KP anda tidak sah.';
+            }
+			/* if (is_numeric($_POST['age']) !== true) {
 				$errors[] = 'Umur hanya mengandungi nombor sahaja.';
-			}
+			} */
 		}
 
         $img = str_replace(" ", "_", $_FILES['file']['name']);
@@ -88,18 +101,18 @@ if (empty($_POST) === false) {
             if(in_array($fileType, $allowTypes)){
                 if($_FILES['file']['size'] > 3000000){
                     $uploadStatus = 0;
-                    $response['message'] = 'Maaf, saiz fail dibenarkan tidak lebih 3Mb.'; 
+                    $errors[] = 'Maaf, saiz fail dibenarkan tidak lebih 3Mb.'; 
                 } else {
                     if(move_uploaded_file($tmp, $targetFilePath)){ 
                         $uploadedFile = $fileName;
                     } else { 
                         $uploadStatus = 0;
-                        $response['message'] = 'Maaf, terdapat ralat semala muat-naik.'; 
+                        $errors[] = 'Maaf, terdapat ralat semala muat-naik.'; 
                     } 
                 }
             } else { 
                 $uploadStatus = 0;
-                $response['message'] = 'Maaf, hanya fail '.implode('/', $allowTypes).' dibenarkan muat-naik.'; 
+                $errors[] = 'Maaf, hanya fail '.implode('/', $allowTypes).' dibenarkan muat-naik.'; 
             }
         } elseif (empty($_FILES['file']['name']) && $_GET['mode'] === 'edit') {
             $uploadedFile = $row['avatar'];
@@ -123,11 +136,22 @@ if (empty($_POST) === false) {
     <h1>Daftar Pengguna</h1>
     <?php	
 		if (empty($_POST) === false && empty($errors) === true) {
+            $currYear = date("Y");
+            if (substr($_POST['noic'],0, 2) > substr(date("Y"),3,2)) {
+                $age = date("Y") - (1900 + substr($_POST['noic'],0 , 2));
+            } else {
+                $age = date("Y") - (2000 + substr($_POST['noic'],0, 2));
+            }
+            if (($_POST['noic'] % 2) == 0) {
+                $gander = "Perempuan";
+            } else {
+                $gander = "Lelaki";
+            }
             $update_data = array(
                 'name' => $_POST['name'],
                 'noic' => $_POST['noic'],
-                'age' => $_POST['age'],
-                'gander' => $_POST['gander'],
+                'age' => $age,
+                'gander' => $gander,
                 'avatar' => $uploadedFile
             );
 			if ($_GET['mode'] === 'edit') {
@@ -153,7 +177,7 @@ if (empty($_POST) === false) {
             <tr>
                 <td>Nama</td>
                 <td><input type="text" id="name" name="name" value="<?php if (empty($errors) === true && $_GET['mode'] === 'edit'){ echo $row['name']; } else if (empty($errors) === false && empty($_POST) === false) { echo $_POST['name']; } ?>"></td>
-                <td rowspan="4">
+                <td rowspan="2">
                     <p>Avatar</p>
                     <p><img src="<?php if (empty($errors) === true && $_GET['mode'] === 'edit'){ if (!empty($row["avatar"])) { echo "images/files/" . $row["avatar"]; } elseif ($row["gander"] == "Lelaki") { echo "images/male.png"; } elseif ($row["gander"] == "Perempuan") { echo "images/female.png"; } else { echo "images/no-photo.png"; } } else if ($_GET['mode'] === 'add') { echo "images/no-photo.png"; } ?>" alt="avatar" height="100" width="100"></p>
                     <input type="file" id="file" name="file">
@@ -163,7 +187,7 @@ if (empty($_POST) === false) {
                 <td>No. KP</td>
                 <td><input type="text" id="noic" name="noic" value="<?php if (empty($errors) === true && $_GET['mode'] === 'edit'){ echo $row['noic']; } else if (empty($errors) === false && empty($_POST) === false) { echo $_POST['noic']; } ?>"></td>
             </tr>
-            <tr>
+            <!-- <tr>
                 <td>Umur</td>
                 <td><input type="number" id="age" name="age" value="<?php if (empty($errors) === true && $_GET['mode'] === 'edit'){ echo $row['age']; } else if (empty($errors) === false && empty($_POST) === false) { echo $_POST['age']; } ?>"></td>
             </tr>
@@ -176,7 +200,7 @@ if (empty($_POST) === false) {
                         <option value='Perempuan' <?php if (empty($errors) === true && $_GET['mode'] === 'edit'){ if ($row['gander'] == 'Perempuan') { echo 'selected="selected"'; } } else if (empty($errors) === false && empty($_POST) === false) { if ($_POST['gander'] == 'Perempuan') { echo 'selected="selected"'; } } ?>>Perempuan</option>
                     </select>
                 </td>
-            </tr>
+            </tr> -->
             <tr>
                 <td colspan="3">
                     <a class="btn warning" href="index.php">Kembali</a>
